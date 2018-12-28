3f49cfa203d0796ebb4f71a2382312d1f2c54b39
jishaoxin <279034231@qq.com>
2018-12-29 03:19:13 +0800

    解决人物移动过程种产生的gc过高问题

--- Assets/Scripts/Game/Containers/MainRoleContainer.cs
+++ Assets/Scripts/Game/Containers/MainRoleContainer.cs
@@ -12,76 +12,84 @@ namespace FF.Game
 {
     public class MainRoleContainer : ContainerIOC, IEntitasSystem
     {
-        //private MainRoleComponent mMainRoleComponent;
-        //private RoleAgentComponent mMainRoleAgentComponent;
-        private EntitasComponentKey mMainRoleAgentComponentKey;
+        MainRoleComponent mMainRoleComponent;
+        RoleAgentComponent mMainRoleAgentComponent;
+        Transform mMainRoleTF;
+        Transform mRoleAgentTF;
+        private int mMainRoleComponentKey;
+        private int mMainRoleAgentComponentKey;
 
         public override void Start()
         {
             base.Start();
 
-            Register<ParamNotice<EntitasComponentKey>, IValueHolder<EntitasComponentKey>>("GetMainCharacterControllerNotice", Pooling<ParamNotice<EntitasComponentKey>>.Instance);
-            Register<ParamNotice<EntitasComponentKey>, IValueHolder<EntitasComponentKey>>("GetMainRoleAgentNotice", Pooling<ParamNotice<EntitasComponentKey>>.Instance);
+            Register<ParamNotice<int>, IValueHolder<int>>("GetMainCharacterControllerNotice", Pooling<ParamNotice<int>>.Instance);
+            Register<ParamNotice<int>, IValueHolder<int>>("GetMainRoleAgentNotice", Pooling<ParamNotice<int>>.Instance);
         }
 
         public override void Finish()
         {
             base.Finish();
 
-            SetAppoint<IValueHolder<EntitasComponentKey>>(this, SetMainCharacterController, "GetMainCharacterControllerNotice");
-            SetAppoint<IValueHolder<EntitasComponentKey>>(this, SetMainRoleAgentComponent, "GetMainRoleAgentNotice");
+            SetAppoint<IValueHolder<int>>(this, SetMainCharacterController, "GetMainCharacterControllerNotice");
+            SetAppoint<IValueHolder<int>>(this, SetMainRoleAgentComponent, "GetMainRoleAgentNotice");
             SetAppoint<IValueHolder<Vector3>>(this, MainRoleWalk);
         }
 
         private void SetMainRoleAgentComponent<I>(ref I target)
         {
-            //mMainRoleAgentComponent = (target as IValueHolder<RoleAgentComponent>).GetValue();
-            mMainRoleAgentComponentKey = (target as IValueHolder<EntitasComponentKey>).GetValue();
+            mMainRoleAgentComponentKey = (target as IValueHolder<int>).GetValue();
         }
 
         private void MainRoleWalk<I>(ref I target)
         {
             Vector3 v = (target as IValueHolder<Vector3>).GetValue();
-            //mMainRoleAgentComponent.faceToMovement = v;
-            //mMainRoleComponent.characterController.SimpleMove(v * 15);
+            mMainRoleAgentComponent.faceToMovement = v;
+            mMainRoleComponent.characterController.SimpleMove(v * 15);
 
-            //if(!mMainRoleAgentComponent.isNeedCheckRoleFaceTo)
-            //{
-            //    mMainRoleAgentComponent.isNeedCheckRoleFaceTo = true;
-            //    GameUpdaterManager.Instance.Add(OnMainRoleFaceingMovement);
-            //}
+            if (!mMainRoleAgentComponent.isNeedCheckRoleFaceTo)
+            {
+                mMainRoleAgentComponent.isNeedCheckRoleFaceTo = true;
+            }
         }
 
         private void OnMainRoleFaceingMovement(int time)
         {
-            RoleAgentComponent agentComponent = GetEntitasComponent<RoleAgentComponent>(ref mMainRoleAgentComponentKey);
-            if (!agentComponent.isNeedCheckRoleFaceTo)
+            if (!mMainRoleAgentComponent.isNeedCheckRoleFaceTo)
             {
-                GameUpdaterManager.Instance.Remove(OnMainRoleFaceingMovement);
                 return;
             }
-
-            //mMainRoleComponent.cachedTF.LookAt(agentComponent.cachedTF.position + agentComponent.faceToMovement);
-            //if(Mathf.Abs(mMainRoleComponent.cachedTF.rotation.y - agentComponent.cachedTF.rotation.y) <= 1)
-            //{
-            //    agentComponent.isNeedCheckRoleFaceTo = false;
-            //}
+            mMainRoleTF.LookAt(mRoleAgentTF.position + mMainRoleAgentComponent.faceToMovement);
+            if (Mathf.Abs(mMainRoleTF.rotation.y - mRoleAgentTF.rotation.y) <= 1)
+            {
+                mMainRoleAgentComponent.isNeedCheckRoleFaceTo = false;
+            }
         }
 
         private void SetMainCharacterController<I>(ref I target)
         {
-            //EntitasComponentKey key = (target as IValueHolder<EntitasComponentKey>).GetValue();
-            //MainRoleComponent component = GetEntitasComponent<MainRoleComponent>(ref key);
-            //mMainRoleComponent = (target as IValueHolder<MainRoleComponent>).GetValue();
+            mMainRoleComponentKey = (target as IValueHolder<int>).GetValue();
+            mMainRoleComponent = GetEntitasComponent<MainRoleComponent>(mMainRoleComponentKey);
 
-            //RoleAgentComponent agentComponent = GetEntitasComponent<RoleAgentComponent>(ref mMainRoleAgentComponentKey);
-            //component.characterController = agentComponent.characterController;
+            mMainRoleAgentComponent = GetEntitasComponent<RoleAgentComponent>(mMainRoleAgentComponentKey);
+            mMainRoleComponent.characterController = mMainRoleAgentComponent.characterController;
 
-            //FruitsMainRoleFSM fsm = new FruitsMainRoleFSM();
-            //fsm.Run(null, FruitMainRoleStateName.STATE_IDLE);
+            if (mMainRoleTF == null)
+            {
+                mMainRoleTF = mMainRoleComponent.cachedTF;
+            }
+            if (mRoleAgentTF == null)
+            {
+                mRoleAgentTF = mMainRoleAgentComponent.cachedTF;
+            }
+
+            FruitsMainRoleFSM fsm = new FruitsMainRoleFSM();
+            fsm.Run(null, FruitMainRoleStateName.STATE_IDLE);
+
+            GameUpdaterManager.Instance.Add(OnMainRoleFaceingMovement);
         }
 
-        public T GetEntitasComponent<T>(ref EntitasComponentKey key) where T : IEntitasComponent
+        public T GetEntitasComponent<T>(int key) where T : IEntitasComponent
         {
             return ComponentAppointer.GetComponet<T>(key);
         }
